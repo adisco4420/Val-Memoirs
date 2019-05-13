1b67bfe98137b7e9fd3b31b7d29fc1648aa4bf49
alabi sodiq <adisco4420@gmail.com>
2019-05-13 16:03:12 +0100

    update app.js with home component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Slider from './components/Slider'
+import Home from './components/home/Home'
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div >
+  <React.Fragment>
         <Header />
         <Slider />
-    </div>
+        <Home />
+        <Footer />
+  </React.Fragment>
   );
 }
 
