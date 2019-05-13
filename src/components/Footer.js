9c11a10f59513b1c41a44a7f63112cb8c3da5af7
alabi sodiq <adisco4420@gmail.com>
2019-05-13 16:03:24 +0100

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+    return (
+      <footer className="bg-danger mt-4">
+        <h6>Val Memoir </h6>
+      </footer>
+    )
+}
+export default Footer;
