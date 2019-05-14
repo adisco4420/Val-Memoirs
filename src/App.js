106e5d78b2c6b4d36a87e3bd880ac335cdeb7380
alabi sodiq <adisco4420@gmail.com>
2019-05-14 10:45:33 +0100

    remove slider and header component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Slider from './components/Slider'
 import Home from './components/home/Home'
 import Footer from './components/Footer';
 
 function App() {
   return (
   <React.Fragment>
-        <Header />
-        <Slider />
         <Home />
         <Footer />
   </React.Fragment>
