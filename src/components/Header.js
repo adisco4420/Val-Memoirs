83ea6b1d7ebd37dac6e958a6cdfbd9494472995e
alabi sodiq <adisco4420@gmail.com>
2019-05-14 09:31:38 +0100

    validate form using react-reactive-form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,80 +1,189 @@
-import React from 'react';
+import React from "react";
+import {
+  FormBuilder,
+  FieldGroup,
+  FieldControl,
+  Validators
+} from "react-reactive-form";
 
+const TextInput = ({ handler, touched, hasError, meta }) => (
+  <div className={`${meta.inputClass || 'col-md-6'} mb-2`}>
+    <input
+      className="form-control form-control-sm"
+      placeholder={`Enter ${meta.label}`}
+      {...handler()}
+    />
+    <span className="text-danger text-monospace" style={{ fontSize: "13px" }}>
+      {touched && hasError("required") && `${meta.label} is required`}
+    </span>
+  </div>
+);
+const TextArea = ({ handler, touched, hasError, meta }) => (
+  <div className="col-md-12">
+    <textarea    className="form-control form-control-sm"  placeholder={`Write ${meta.label}....`}
+         {...handler()} rows="5" cols="10"
+    >
+    </textarea>
+    <span className="text-danger text-monospace" style={{ fontSize: "13px" }}>
+      {touched && hasError("required") && `${meta.label} is required`}
+    </span>
+  </div>
+);
+const SelectBox = ({ handler }) => (
+  <div className="col-md-6">
+    <select className="form-control form-control-sm" {...handler()}>
+      <option value="" disabled>
+        Select Gender
+      </option>
+      <option value="male">Male</option>
+      <option value="female">Female</option>
+    </select>
+  </div>
+);
 
-
-function Header() {
-    return(
-        <React.Fragment>
+class Header extends React.Component {
+  loginForm = FormBuilder.group({
+    firstName: ["", Validators.required],
+    lastName: ["", Validators.required],
+    username: ["", Validators.required],
+    gender: ["", Validators.required],
+    headline: ["", Validators.required],
+    story: ["", Validators.required]
+  });
+  handleReset = () => {
+    this.loginForm.reset();
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Form values", this.loginForm.value);
+    this.props.saveStory(this.loginForm.value)
+  };
+  render() {
+    return (
+      <React.Fragment>
         <nav className="navbar navbar-expand-lg navbar-light bg-danger text-light">
-            <a className="navbar-brand text-light" href="#">Val Memoirs</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+          <a className="navbar-brand text-light" href="/">
+            Val Memoirs
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-      </li>
-
-    </ul>
-    <div className="my-2 my-lg-0">
-        <button type="button" data-toggle="modal" data-target="#exampleModalCenter" className="btn btn-light btn-sm"> <i className="fa fa-plus-circle"></i> Add Story</button>
-        <button className="btn text-light"><i className="fa fa-lightbulb"></i> User Guide</button>
-    </div>
-  </div>
-
-
-</nav>
-<div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog"  aria-hidden="true">
-  <div className="modal-dialog modal-dialog-centered" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLongTitle">Share New Story</h5>
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div className="modal-body">
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-6 mb-2">
-                    <input className="form-control form-control-sm" placeholder="first name" type="text" />
-                </div>
-                <div className="col-md-6 mb-2">
-                    <input className="form-control form-control-sm" placeholder="last name" type="text" />
-                </div>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
+            </ul>
+            <div className="my-2 my-lg-0">
+              <button
+                type="button"
+                data-toggle="modal"
+                data-target="#exampleModalCenter"
+                className="btn btn-light btn-sm"
+              >
+                {" "}
+                <i className="fa fa-plus-circle" /> Add Story
+              </button>
+              <button className="btn text-light">
+                <i className="fa fa-lightbulb" /> User Guide
+              </button>
             </div>
-            <div className="row">
-                <div className="col-md-6 mb-2">
-                    <input className="form-control form-control-sm" placeholder="username" type="text" />
+          </div>
+          <div
+            className="modal fade"
+            id="exampleModalCenter"
+            tabIndex="-1"
+            role="dialog"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog modal-dialog-centered" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5
+                    className="modal-title text-dark"
+                    id="exampleModalLongTitle"
+                  >
+                    Share New Story
+                  </h5>
+                  <button
+                    onClick={this.handleReset}
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
                 </div>
-                <div className="col-md-6 mb-2">
-                <select className="form-control form-control-sm">
-                    <option>Male</option>
-                    <option>Female</option>
-                </select>
+                <div className="modal-body">
+                  <div className="container-fluid">
+                    <FieldGroup
+                      control={this.loginForm}
+                      render={({ get, invalid }) => (
+                        <form onSubmit={this.handleSubmit}>
+                          <div className="row">
+                            <FieldControl
+                              name="firstName"
+                              render={TextInput}
+                              meta={{ label: "First Name" }}
+                            />
+
+                            <FieldControl
+                              name="lastName"
+                              render={TextInput}
+                              meta={{ label: "Last Name" }}
+                            />
+                          </div>
+
+                          <div className="row">
+                            <FieldControl
+                              name="username"
+                              render={TextInput}
+                              meta={{ label: "Username" }}
+                            />
+                            <FieldControl name="gender" render={SelectBox} />
+                          </div>
+                          <div className="row">
+                            <FieldControl
+                              name="headline"
+                              render={TextInput}
+                              meta={{ label: "Headline", inputClass: "col-md-12" }}
+                            />
+                            <FieldControl name="story" render={TextArea} meta={{label: "A Story"}} />
+                          </div>
+
+                          <div className="modal-footer">
+                            <button
+                              disabled={invalid}
+                              type="submit"
+                              className="btn btn-primary"
+                            >
+                              Save changes
+                            </button>
+                          </div>
+                        </form>
+                      )}
+                    />
+                  </div>
                 </div>
+              </div>
             </div>
-            <div className="row">
-                <div className="col-md-12 mb-2">
-                <input className="form-control form-control-sm" placeholder="headline" type="text" />
-                </div>
-                <div className="col-md-12 mb-2">
-                <textarea className="form-control form-control-sm" placeholder="story..." rows="5" cols="10" ></textarea>
-                </div>
-            </div>
-        </div>
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-</React.Fragment>
-    )
+          </div>
+        </nav>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Header;
