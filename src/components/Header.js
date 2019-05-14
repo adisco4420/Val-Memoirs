f4dd6635c0b137a01a27e6cf0fd8b4e86ce48167
alabi sodiq <adisco4420@gmail.com>
2019-05-14 14:47:17 +0100

    user guide

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,8 +20,8 @@ const TextInput = ({ handler, touched, hasError, meta }) => (
 );
 const TextArea = ({ handler, touched, hasError, meta }) => (
   <div className="col-md-12">
-    <textarea    className="form-control form-control-sm"  placeholder={`Write ${meta.label}....`}
-         {...handler()} rows="5" cols="10"
+    <textarea className="form-control form-control-sm" placeholder={`Write ${meta.label}....`}
+      {...handler()} rows="5" cols="10"
     >
     </textarea>
     <span className="text-danger text-monospace" style={{ fontSize: "13px" }}>
@@ -58,8 +58,7 @@ class Header extends React.Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-    // console.log("Form values", this.loginForm.value);
-    if(this.loginForm.valid) {
+    if (this.loginForm.valid) {
       this.props.saveStory(this.loginForm.value);
       this.handleReset();
     }
@@ -86,7 +85,7 @@ class Header extends React.Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="/">
+                <a className="nav-link text-light" href="/">
                   Home <span className="sr-only">(current)</span>
                 </a>
               </li>
@@ -98,10 +97,10 @@ class Header extends React.Component {
                 data-target="#exampleModalCenter"
                 className="btn btn-light btn-sm"
               >
-                {" "}
                 <i className="fa fa-plus-circle" /> Add Story
               </button>
-              <button className="btn text-light">
+              <button data-toggle="modal" data-target="#userGuide"
+               className="btn text-light">
                 <i className="fa fa-lightbulb" /> User Guide
               </button>
             </div>
@@ -166,12 +165,12 @@ class Header extends React.Component {
                               render={TextInput}
                               meta={{ label: "Headline", inputClass: "col-md-12" }}
                             />
-                            <FieldControl name="story" render={TextArea} meta={{label: "A Story"}} />
+                            <FieldControl name="story" render={TextArea} meta={{ label: "A Story" }} />
                           </div>
 
                           <div className="modal-footer">
                             <button
-                             data-toggle="modal" data-target="#exampleModalCenter"
+                              data-toggle="modal" data-target="#exampleModalCenter"
                               disabled={invalid}
                               type="submit"
                               className="btn btn-primary"
@@ -188,6 +187,51 @@ class Header extends React.Component {
             </div>
           </div>
         </nav>
+
+          <div
+            className="modal fade"
+            id="userGuide"
+            tabIndex="-1"
+            role="dialog"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog modal-dialog-centered" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5
+                    className="modal-title text-dark">
+                   <i className="fa fa-lightbulb"></i> User Guide
+                  </h5>
+                  <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                <div className="container-fluid">
+                  <div className="row">
+                      <div className="col-md-12">
+                        <h6>Guideline on how to share a story </h6>
+                        <ul>
+                          <li>Click on the <strong><i className="fa fa-plus-circle"></i> Add Story</strong> button on the header navbar</li>
+                          <li>Fill all required input: <em>First Name, Last Name, Gender, Username, Headline, Story</em></li>
+                          <li>A user can only share a story once</li>
+                          <li>You can <b><i className="fa fa-thumbs-up"></i> like</b> any post unlimited amount of times</li>
+                          <li>Click <b>read more</b> button to view detail of a story</li>
+                        </ul>
+                        <p></p>
+                      </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+     
       </React.Fragment>
     );
   }
